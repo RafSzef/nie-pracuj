bf7c3d61bb642a811f5cd73ff055a4784dfc0e5d
RafSzef <raf.szefler@gmail.com>
2022-10-01 11:11:42 +0200

    initial commit cleanup

--- src/main/java/pl/pracuj/niepracuj/NiePracujApplication.java
+++ src/main/java/pl/pracuj/niepracuj/NiePracujApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NiePracujApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NiePracujApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NiePracujApplication.class, args);
+    }
 
 }
