61baced3e7d439196bddc9193dd1fb574e0de443
Rafał Szefler <59801632+RafSzef@users.noreply.github.com>
2022-10-02 13:28:23 +0200

    ferature/zadanie_4 Dodawania i pobieranie technologii

---
+++ src/main/java/pl/niepracuj/model/mapper/TechnologyMapper.java
@@ -0,0 +1,27 @@
+package pl.niepracuj.model.mapper;
+
+import org.springframework.stereotype.Component;
+import pl.niepracuj.model.dto.TechnologyDto;
+import pl.niepracuj.model.entity.Technology;
+
+@Component
+public class TechnologyMapper {
+    public TechnologyDto toDto(Technology technology) {
+        return TechnologyDto.builder()
+                .id(technology.getId()  )
+                .name(technology.getName())
+                .build();
+    }
+
+    public Technology toEntity(TechnologyDto technologyDto) {
+        return Technology.builder()
+                .id(technologyDto.getId())
+                .name(technologyDto.getName())
+                .build();
+    }
+    public Technology toNewEntity(TechnologyDto technologyDto) {
+        return Technology.builder()
+                .name(technologyDto.getName())
+                .build();
+    }
+}
